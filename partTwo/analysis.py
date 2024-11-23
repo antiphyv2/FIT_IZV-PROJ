f9a0074267a54fefdfb60444e032a0a66ac3809c
antiphy <antiphycz@gmail.com>
2024-11-23 16:31:23 +0100

    sth

--- partTwo/analysis.py
+++ partTwo/analysis.py
@@ -42,7 +42,7 @@ def load_data(filename : str, ds : str) -> pd.DataFrame:
 
 def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
 
-    regionDict = {
+    regions = {
         0: "PHA", 1: "STC", 2: "JHC", 3: "PLK",
         4: "ULK", 5: "HKK", 6: "JHM", 7: "MSK",
         14: "OLK", 15: "ZLK", 16: "VYS", 17: "PAK",
@@ -51,18 +51,69 @@ def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
 
     newDf = df.copy()
     newDf["date"] = pd.to_datetime(newDf["p2a"], format="%d.%m.%Y", errors="coerce")
-    newDf["region"] = newDf["p4a"].map(regionDict)
+    newDf["region"] = newDf["p4a"].map(regions)
     newDf = newDf.drop_duplicates(subset='p1', keep=False)
 
     if(verbose):
         print(f'new_size={(newDf.memory_usage(deep=True).sum() / (1000 * 1000)):.1f} MB')
-    print(newDf)
     return newDf
 
 # Ukol 3: počty nehod v jednotlivých regionech podle stavu vozovky
 def plot_state(df: pd.DataFrame, fig_location: str = None,
                     show_figure: bool = False):
-    pass
+
+    roadStates = {
+        1: "povrch suchý",
+        2: "povrch suchý",
+        3: "povrch mokrý",
+        4: "na vozovce je bláto",
+        5: "na vozovce je náledí, ujetý sníh",
+        6: "na vozovce je náledí, ujetý sníh",
+    }
+
+    df["roadStates"] = df["p16"].map(roadStates)
+    # roadsWithRegions = df.groupby(["region", "roadStates"]).size().unstack(level="roadStates")
+    roadsWithRegions = df.groupby(["region", "roadStates"]).size().reset_index(name="count")
+    print(roadsWithRegions)
+    # print(grouped)
+
+
+    
+    fig, axes = plt.subplots(2, 2, figsize=(10, 8))
+    axes = axes.flatten()
+    
+    # plt.subplots_adjust(hspace=0.3)
+    # plt.subplots_adjust(wspace=0.3)
+
+    fig.suptitle("Počtet nehod v jednotlivých krajích dle stavu vozovky")
+
+    roadStateList = roadsWithRegions["roadStates"].unique()
+    for i, axe in enumerate(axes):
+
+        currentRoadState = roadsWithRegions[roadsWithRegions["roadStates"] == roadStateList[i]]
+        sns.barplot(data=currentRoadState, x="region", y="count", ax=axe, palette="Blues", hue="count")
+
+        axe.set_title(roadStateList[i])
+        axe.tick_params(axis='x', rotation=45)
+        axe.set_xlabel("Kraj")
+        axe.set_ylabel("Počet nehod")
+
+        
+
+    # g = sns.catplot(data=roadsWithRegions, x="region", y="count", col="roadStates", kind="bar", col_wrap=2, sharey=False, palette="Blues", hue="count")
+    # g.figure.suptitle("Počet nehod v jednotlivých krajích dle stavu vozovky")
+    # g.set_titles("{col_name}")
+    # g.tick_params(axis='x', rotation=45)
+    # g.set_axis_labels("Kraj", "Počet nehod")
+
+    # for ax in g.axes.flat:
+    #     ax.set_xlabel("Kraj")
+    #     ax.set_ylabel("Počet nehod")
+
+    # g.tight_layout()
+    # plt.subplots_adjust(hspace=0.2)
+    plt.tight_layout()
+    plt.show()
 
 # Ukol4: alkohol a následky v krajích
 def plot_alcohol(df: pd.DataFrame, df_consequences : pd.DataFrame, 
@@ -86,7 +137,7 @@ if __name__ == "__main__":
     # print(f"Table2:\n{df_consequences}")
     df2 = parse_data(df, True)
     
-    # plot_state(df2, "01_state.png")
+    plot_state(df2, "01_state.png")
     # plot_alcohol(df2, df_consequences, "02_alcohol.png", True)
     # plot_type(df2, "03_type.png")
 
